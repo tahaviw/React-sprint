8ed5fd72f27c7efd7c5ae59421d40ab385591f4d
Taha <tahaviwtahtouhihihi@gmail.com>
2026-08-16 23:52:17 +0100

    Destructure & use Children

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,11 @@
 import ProfilePic from './assets/Profile_Pic.jpg'
 
-function Card({title, desciption}) {
+function Card({title, children}) {
     return(
         <div className="card">
             <img className="card-image" src={ProfilePic} alt="profile picture" />
             <h2 className="card-title" >{title}</h2>
-            <p className="card-paragraph">{desciption}</p>
+            <p className="card-paragraph">{children}</p>
         </div>
     )
 }
--- src/CardList.jsx
+++ src/CardList.jsx
@@ -3,9 +3,14 @@ import Card from './Card.jsx'
 function CardList() {
     return(
         <div className='card-list'>
-            <Card title="Taha Belghiti" desciption="I'm a software Dev interested in AI"/>
-            <Card title="Molay Ahmed" desciption="I'm a little boy I love Cars"/>
-            <Card title="Salsabil" desciption="I'm the middle child"/>
+            <Card title="Taha Belghiti" children="I'm a software Dev interested in AI"/>
+            <Card title="Molay Ahmed" children="I'm a little boy I love Cars"/>
+            <Card title="Salsabil" children="I'm the middle child"/>
+            <Card title="Special Card">
+                <p>This paragraph is passed as children</p>
+                <button>Click me</button>
+            </Card>
+
         </div>
     );
 }
