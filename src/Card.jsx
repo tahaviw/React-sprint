35fdfd5f93f4baa8f29dfcce08d849722690882f
Taha <tahaviwtahtouhihihi@gmail.com>
2026-08-28 22:46:12 +0100

    appliment useState in LikeBtn

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,14 @@
 import ProfilePic from './assets/Profile_Pic.jpg'
+import LikeButton from './LikeButton'
 
 function Card({title, children}) {
+
     return(
         <div className="card">
             <img className="card-image" src={ProfilePic} alt="profile picture" />
             <h2 className="card-title" >{title}</h2>
             <div className="card-body">{children}</div>
+            <LikeButton/>
         </div>
     )
 }
---
+++ src/LikeButton.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function LikeButton () {
+    const [likes, setLikes] = useState(0)
+
+    return(
+        <button onClick={() => setLikes(likes + 1)}>{likes}</button>
+    );
+}
+
+export default LikeButton
