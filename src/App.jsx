7ff0704169e26e335a4e4ae59faee37f774d1656
Taha <tahaviwtahtouhihihi@gmail.com>
2026-09-12 12:39:15 +0100

    add Ternary Toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import CardList from './CardList.jsx'
@@ -6,12 +7,17 @@ import ContactForm from './ContactForm.jsx'
 
 
 function App() {
+  const [showForm,setShowForm] = useState(false)
+  let messageP = "This form is currently hidden. To reveal the form and continue with your application, please ensure you have completed all required fields in the previous section and agreed to the terms and conditions." 
   return (
     <>
       <Header/>
       <SearchBar/>
+      <button onClick={()=> setShowForm (!showForm)}>
+        {showForm ? 'Hide Form' : 'Show Form'}
+      </button>
       <CardList/>
-      <ContactForm/>
+      {showForm ? <ContactForm/> : messageP}
       <Footer/>
     </>
   );
