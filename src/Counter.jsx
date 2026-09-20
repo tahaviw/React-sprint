89cd5efd933b2831680c9229df27df55b530f032
Taha <tahaviwtahtouhihihi@gmail.com>
2026-09-20 21:48:18 +0000

    style counter btn & use Liquide Glass

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,11 +2,18 @@ import { useState, useEffect } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
-  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log("Component loaded");
+  }, []);
   return (
-    <button onClick={() => setCount(count + 1)}>
-      Add to the Count: {count}
-    </button>
+    <div className="counter-card">
+      <p>Current count: {count}</p>
+
+      <button className="liquid-glass-btn" onClick={() => setCount(count + 1)}>
+        <span className="liquid-shine"></span>
+        Increment
+      </button>
+    </div>
   );
 }
 
